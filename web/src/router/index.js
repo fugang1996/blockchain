a7445ddafb18c21f13d158dec0469b1150fac628
fugang <2218889604@qq.com>
2021-06-02 16:54:47 +0800

    ccc

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -4,7 +4,7 @@
  * @Author: fugang
  * @Date: 2021-06-02 11:56:31
  * @LastEditors: fugang
- * @LastEditTime: 2021-06-02 13:53:02
+ * @LastEditTime: 2021-06-02 16:11:52
  */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
@@ -35,6 +35,11 @@ const routes = [
     path: '/invitation',
     name: 'Invitation',
     component: () => import('../views/Invitation.vue')
+  },
+  {
+    path: "/user",
+    name: 'User',
+    component: () => import('../views/User.vue')
   }
 ]
 
---
+++ web/src/views/User.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Descripttion: no
+ * @version: 1.0.0
+ * @Author: fugang
+ * @Date: 2021-06-02 16:11:07
+ * @LastEditors: fugang
+ * @LastEditTime: 2021-06-02 16:19:37
+-->
+<template>
+  <div class="user-container">
+    <div class="user-header">
+      <div class="user-header-left">
+        <img src="../assets/user/userlogo.png" alt="">
+      </div>
+      <div class="user-header-right"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+
+</style>
